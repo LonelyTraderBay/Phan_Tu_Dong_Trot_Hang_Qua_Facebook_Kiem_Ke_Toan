b4ff033061f79b00225f40a4b4fa6a7bb5d68e49
LonelyTraderBay <jokerse7en7@gmail.com>
2026-07-24 17:00:58 +0700

    chore: initialize pnpm turborepo monorepo root

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.next/**',
+      '**/.turbo/**',
+      '**/coverage/**',
+    ],
+  },
+];
